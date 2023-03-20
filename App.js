6559f6bf0d3deaf258f03073500a34425310a09b
prasanth pulavarthi <prasanthpulavarthi19@gmail.com>
2023-03-20 15:27:17 +0530

    episode-1

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
 const heading = React.createElement("h1", {}, "Hello world from react");
-const heading1 = React.createElement("h1", {}, "Hello world from react1");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const roott = ReactDOM.createRoot(document.getElementById("roott"));
 root.render(heading);
-roott.render(heading1);

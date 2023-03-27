dc03d606dd19881b3406b370edc8555f8aa76480
prasanth pulavarthi <prasanthpulavarthi19@gmail.com>
2023-03-27 11:06:45 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,4 +1,21 @@
-const heading = React.createElement("h1", {}, "Hello world from react");
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "h1",
+  { id: "head" },
+  "Hello world from react 1"
+);
+console.log(heading);
+const heading1 = <h1 id="head">jsx element</h1>;
+console.log(heading1);
+const Compo = () => <h1>component composition</h1>;
+const Comp = () => {
+  return (
+    <>
+      <Compo />
+      <h1>function component</h1>
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Comp />);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 #React
+
+#parcel
+-Dev build
+-local server
+-HMR="hot module replacement"
+-File Watching algorithm - written in c#
+-caching - faster builds
+-image optimazation
+-bundling
+-compress
+-consistent hashing
+-code splitting
+-differential bundling
+-diagnostic -(shows error in a good way)
+-error handling
+-HTTPS
+-Tree shaking - (remove used code)
